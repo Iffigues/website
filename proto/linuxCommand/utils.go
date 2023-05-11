ed8cd6bc4dbbeb420c314087d13dc1b5ae19c469
Debian <debian@vps683807.ovh.net>
2023-05-11 00:36:46 +0000

    ez

--- proto/linuxCommand/utils.go
+++ proto/linuxCommand/utils.go
@@ -1,6 +1,5 @@
 package linuxCommand
 
-import ("fmt")
 
 func addOptTab(r []string, e ...string) (b []string) {
 	for _, val := range e {
@@ -10,14 +9,11 @@ func addOptTab(r []string, e ...string) (b []string) {
 }
 
 func startExec(a string, t []string) (e *Responses, err error) {
-	fmt.Println(a, t)
 	e = new(Responses)
 	stdout, stderr, erro := Exec(a, t)
 	if erro != nil {
-		fmt.Println("hihi", erro, stderr)
 		return nil, erro
 	}
 	e.StdoutResponse, e.StderrResponse = stdout.String(), stderr.String()
-	fmt.Println("haha", string(e.StderrResponse))
 	return e, nil
 }
